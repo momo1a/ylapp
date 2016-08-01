0dfc4aab47089e23444d9a44350e6fa42b504807
moshiyou-home <momo1a@qq.com>
2016-08-01 23:39:38 +0800

    【表结构】

---
+++ docs/sql/table/order.sql
@@ -0,0 +1,21 @@
+ # 订单表
+DROP TABLE IF EXISTS  `YL_order`;
+CREATE TABLE `YL_order` (
+  `oid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `buyerId` int(11) unsigned not null default '0' comment '购买人id',
+  `buyerName` varchar(25) not null default '' comment '购买人名称',
+  `buyerSex` tinyint(3) unsigned not null default '0' comment '购买人性别1男 2女',
+  `buyerTel` varchar(20) not null default '' comment '购买人电话',
+  `buyerBrithday` int(11) unsigned not null default '0' comment '购买人出生日期',
+  `packageId` int(11) unsigned not null default '0' comment '购买套餐id',
+  `packageTitle` varchar(255) not null default '' comment '购买套餐名称',
+  `price` decimal(9,2) unsigned not null default '0' comment '套餐价格',
+  `type` tinyint(3) unsigned not null default '0' comment '类型：1疫苗接种，2基因检测',
+  `dateline` int(11) unsigned not null default '0' comment '下单时间',
+  `status` tinyint(3) unsigned not null default '0' comment '状态：1.状态有待处理，2已支付，3未支付，4已通知，5完成',
+  PRIMARY KEY (`oid`),
+  KEY `status` (`status`) USING BTREE,
+  KEY `packageId` (`packageId`) USING BTREE,
+  KEY `buyerId` (`buyerId`) USING BTREE,
+  KEY `type` (`type`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
---
+++ docs/sql/table/take_cash.sql
@@ -0,0 +1,20 @@
+ # 提现表
+DROP TABLE IF EXISTS  `YL_take_cash`;
+CREATE TABLE `YL_take_cash` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(11) unsigned not null default '0' comment '提现人id',
+  `name` varchar(25) not null default '' comment '提现人昵称',
+  `bank` varchar(125) not null default '' comment '开户银行',
+  `address` varchar(125) not null default '' comment '开户地区',
+  `cardNum` varchar(50) not null default '' comment '银行卡号',
+  `realName` varchar(18) not null default '' comment '真实姓名',
+  `identity` varchar(30) not null default '' comment '身份证',
+  `amount` DECIMAL(9,2) unsigned not null default '0' comment '提现金额',
+  `userType` tinyint(3) unsigned not null default '0' comment '用户类型1用户端，2医生端',
+  `dateline` int(11) unsigned not null default '0' comment '提现时间',
+  `status` tinyint(3) unsigned not null default '0' comment '状态：0.待处理，1已确认，2驳回',
+  PRIMARY KEY (`id`),
+  KEY `status` (`status`) USING BTREE,
+  KEY `uid` (`uid`) USING BTREE,
+  KEY `userType` (`userType`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='提现表';

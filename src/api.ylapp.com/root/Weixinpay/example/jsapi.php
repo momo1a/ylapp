8116ddc7a316d0d25ace4c1a309b1bcd168095d9
moshiyou-home <momo1a@qq.com>
2016-10-04 20:13:55 +0800

    在

--- src/api.ylapp.com/root/Weixinpay/example/jsapi.php
+++ src/api.ylapp.com/root/Weixinpay/example/jsapi.php
@@ -35,7 +35,7 @@ $input->SetTrade_type("JSAPI");
 $input->SetOpenid($openId);
 $order = WxPayApi::unifiedOrder($input);
 echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
-printf_info($order);
+printf_info($order);exit;
 $jsApiParameters = $tools->GetJsApiParameters($order);
 
 //获取共享收货地址js函数参数

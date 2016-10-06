2f45cf2938b856d991d917c8630276e1a58f60de
moshiyou-home <momo1a@qq.com>
2016-10-06 21:20:41 +0800

    佛挡杀佛的是

--- src/api.ylapp.com/controllers/notice.php
+++ src/api.ylapp.com/controllers/notice.php
@@ -139,6 +139,7 @@ class Notice extends CI_Controller
         );
         $this->load->library('alipay/AliPay', $config, 'alipay');   // 支付宝支付调用类
         $verifyResult = $this->alipay->notify();
+        var_dump($verifyResult);
         if ($verifyResult) {//验证成功
             /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             //请在这里加上商户的业务逻辑程序代码

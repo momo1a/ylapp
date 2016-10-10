318560de99cc4e1ef6009571acf8aa1ba517b8ac
莫世友 <momo1a@qq.com>
2016-10-10 09:13:42 +0800

    第三方

--- src/api.ylapp.com/root/wxlogin/index.php
+++ src/api.ylapp.com/root/wxlogin/index.php
@@ -8,5 +8,5 @@
 
 $appid = 'wx1a13456d65204e33';
 
-header('location:https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=123.207.87.83:8080/wxlogin/oauth.php&response_type=code&scope=snsapi_userinfo&state=123&connect_redirect=1#wechat_redirect');
+header('location:https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http://123.207.87.83:8080/wxlogin/oauth.php&response_type=code&scope=snsapi_userinfo&state=123&connect_redirect=1#wechat_redirect');
 

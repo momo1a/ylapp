643c87f4730778edd502a1f574e2afd580083735
莫世友 <momo1a@qq.com>
2016-10-10 11:09:41 +0800

    发的所发生的

--- src/api.ylapp.com/root/wxlogin/oauth.php
+++ src/api.ylapp.com/root/wxlogin/oauth.php
@@ -1,35 +1,31 @@
 <?php
-$code = $_GET['code'];
-$state = $_GET['state'];
-//换成自己的接口信息
-$appid = 'wx1a13456d65204e33';
-$appsecret = '5b269d029775647975de2e96339a1d29';
-if (empty($code)) $this->error('授权失败');
-$token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$appsecret.'&code='.$code.'&grant_type=authorization_code';
-$token = json_decode(file_get_contents($token_url));
-if (isset($token->errcode)) {
-    echo '<h1>错误：</h1>'.$token->errcode;
-    echo '<br/><h2>错误信息：</h2>'.$token->errmsg;
-    exit;
+if($_GET['state']!=$_SESSION["wx_state"]){
+    exit("5001");
 }
-$access_token_url = 'https://api.weixin.qq.com/sns/oauth2/refresh_token?appid='.$appid.'&grant_type=refresh_token&refresh_token='.$token->refresh_token;
-//转成对象
-$access_token = json_decode(file_get_contents($access_token_url));
-if (isset($access_token->errcode)) {
-    echo '<h1>错误：</h1>'.$access_token->errcode;
-    echo '<br/><h2>错误信息：</h2>'.$access_token->errmsg;
-    exit;
-}
-$user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token->access_token.'&openid='.$access_token->openid.'&lang=zh_CN';
-//转成对象
-$user_info = json_decode(file_get_contents($user_info_url));
-if (isset($user_info->errcode)) {
-    echo '<h1>错误：</h1>'.$user_info->errcode;
-    echo '<br/><h2>错误信息：</h2>'.$user_info->errmsg;
-    exit;
-}
-//打印用户信息
-echo '<pre>';
-print_r($user_info);
-echo '</pre>';
+$AppID = 'wx1a13456d65204e33';
+$AppSecret = '5b269d029775647975de2e96339a1d29';
+$url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$AppID.'&secret='.$AppSecret.'&code='.$_GET['code'].'&grant_type=authorization_code';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_URL, $url);
+$json =  curl_exec($ch);
+curl_close($ch);
+
+$arr=json_decode($json,1);
+
+//得到 access_token 与 openid
+print_r($arr);
+
+$url='https://api.weixin.qq.com/sns/userinfo?access_token='.$arr['access_token'].'&openid='.$arr['openid'].'&lang=zh_CN';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_URL, $url);
+$json =  curl_exec($ch);
+curl_close($ch);
+$arr=json_decode($json,1);
+//得到 用户资料
+print_r($arr);
 ?>

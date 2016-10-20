4e4df7e71855c5639f3df9bc3929a1b041f1acc1
莫世友 <momo1a@qq.com>
2016-10-20 14:10:54 +0800

    防守打法第三

--- docs/sql/table/system_setting.sql
+++ docs/sql/table/system_setting.sql
@@ -4,5 +4,6 @@ CREATE TABLE `YL_system_setting` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `settingKey` VARCHAR(25) NOT NULL DEFAULT '' comment '设置key',
   `settingValue` VARCHAR(60) NOT NULL DEFAULT '' comment '设置value',
+  UNIQUE KEY (`settingKey`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统设置表';

3c097259fa8514596bfa68f1f855f3aad49fffaa
莫世友 <momo1a@qq.com>
2016-10-21 17:52:57 +0800

    惹我热热我

--- src/common/libraries/aliyun/test.php
+++ src/common/libraries/aliyun/test.php
@@ -18,39 +18,74 @@ class Test
     protected $_sectet = 'a3af895021da1f5784d872f14c7124c0';
 
 
+    /**
+     * 淘宝sdk对象
+     * @var null
+     */
+    protected static $_TopClientInstance = null;
+
+    /**
+     * 用户添加请求对象
+     * @var null
+     */
+    protected static $_userAddRequest = null;
+
+
+    /**
+     * 获取用户请求对象
+     * @var null
+     */
+    protected static $_usersGetRequest = null;
+
+
+    /**
+     * 初始化
+     */
     public function __construct(){
-        $c = new TopClient;
-        $c->appkey = $this->_appKye;
-        $c->secretKey = $this->_sectet;
-        $req = new OpenimUsersAddRequest;
-        $userinfos['nick']="king";
-        $userinfos['icon_url']="http://xxx.com/xxx";
-        $userinfos['email']="uid@taobao.com";
-        $userinfos['mobile']="18600000000";
-        $userinfos['taobaoid']="tbnick123";
-        $userinfos['userid']="imuser123";
-        $userinfos['password']="123456a";
-        $userinfos['remark']="demo";
-        $userinfos['extra']="{}";
-        $userinfos['career']="demo";
-        $userinfos['vip']="{}";
-        $userinfos['address']="demo";
-        $userinfos['name']="demo";
-        $userinfos['age']="123";
-        $userinfos['gender']="M";
-        $userinfos['wechat']="demo";
-        $userinfos['qq']="demo";
-        $userinfos['weibo']="demo";
-        $req->setUserinfos(json_encode($userinfos));
-        $resp = $c->execute($req);
-        var_dump($resp);
+        self::$_TopClientInstance = new TopClient;
+        self::$_TopClientInstance->appkey = $this->_appKye;
+        self::$_TopClientInstance->secretKey = $this->_sectet;
+        self::$_userAddRequest = new OpenimUsersAddRequest;
+        self::$_usersGetRequest = new OpenimUsersGetRequest;
     }
 
+    /**
+     * 添加用户
+     * @param array $userInfo 用户参数
+     */
+    public function userAdd($userInfo){
+        $userinfos['nick']="myking";
+        //$userinfos['icon_url']="http://xxx.com/xxx";
+        //$userinfos['email']="uid@taobao.com";
+        //$userinfos['mobile']="18600000000";
+        //$userinfos['taobaoid']="tbnick123";
+        $userinfos['userid']="mylolol";
+        $userinfos['password']="123456a";
+        /* $userinfos['remark']="demo";
+         $userinfos['extra']="{}";
+         $userinfos['career']="demo";
+         $userinfos['vip']="{}";
+         $userinfos['address']="demo";
+         $userinfos['name']="demo";
+         $userinfos['age']="123";
+         $userinfos['gender']="M";
+         $userinfos['wechat']="demo";
+         $userinfos['qq']="demo";
+         $userinfos['weibo']="demo";*/
+        self::$_userAddRequest->setUserinfos(json_encode($userInfo));
+        $response = self::$_TopClientInstance->exec(self::$_userAddRequest);
+        return $response;
+    }
 
-
-
-
-
+    /**
+     * 获取用户信息
+     * @param $userId
+     */
+    public function getUserInfo($userId){
+        self::$_usersGetRequest->setUserids($userId);
+        $response = self::$_TopClientInstance->execute(self::$_usersGetRequest);
+        return $response;
+    }
 
 
 

2e10b2595c178b71e78ff7615359c426c9c149dc
莫世友 <momo1a@qq.com>
2016-10-27 10:03:55 +0800

    发到范德萨范德萨发是

--- src/common/libraries/union/sdk/SDKConfig.php
+++ src/common/libraries/union/sdk/SDKConfig.php
@@ -3,7 +3,8 @@ namespace com\unionpay\acp\sdk;
  
 // ######(以下配置为PM环境：入网测试环境用，生产环境配置见文档说明)#######
 // 签名证书路径
-const SDK_SIGN_CERT_PATH = '/mnt/myweb/ylapp/src/common/libraries/union/certs/acp_test_sign.pfx';
+//const SDK_SIGN_CERT_PATH = '/mnt/myweb/ylapp/src/common/libraries/union/certs/acp_test_sign.pfx';
+const SDK_SIGN_CERT_PATH = '/usr/local/nginx/html/ylapp/src/common/libraries/union/certs/acp_test_sign.pfx';
 
 // 签名证书密码
 const SDK_SIGN_CERT_PWD = '000000';
@@ -12,10 +13,12 @@ const SDK_SIGN_CERT_PWD = '000000';
 const  SDK_MER_ID = '826440153990003';
 
 // 密码加密证书（这条一般用不到的请随便配）
-const SDK_ENCRYPT_CERT_PATH = '/mnt/myweb/ylapp/src/common/libraries/union/certs/acp_test_enc.cer';
+//const SDK_ENCRYPT_CERT_PATH = '/mnt/myweb/ylapp/src/common/libraries/union/certs/acp_test_enc.cer';
+const SDK_ENCRYPT_CERT_PATH = '/usr/local/nginx/html/ylapp/src/common/libraries/union/certs/acp_test_enc.cer';
 
 // 验签证书路径（请配到文件夹，不要配到具体文件）
-const SDK_VERIFY_CERT_DIR = '/mnt/myweb/ylapp/src/common/libraries/union/certs/';
+//const SDK_VERIFY_CERT_DIR = '/mnt/myweb/ylapp/src/common/libraries/union/certs/';
+const SDK_VERIFY_CERT_DIR = '/usr/local/nginx/html/ylapp/src/common/libraries/union/certs/';
 
 // 前台请求地址
 const SDK_FRONT_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/frontTransReq.do';
@@ -45,10 +48,10 @@ const SDK_FRONT_NOTIFY_URL = 'http://localhost:8085/upacp_demo_app/demo/api_05_a
 const SDK_BACK_NOTIFY_URL = 'http://222.222.222.222/upacp_demo_app/demo/api_05_app/BackReceive.php';
 
 //文件下载目录 
-const SDK_FILE_DOWN_PATH = '/mnt/myweb/ylapp/src/common/libraries/union/files/';
+const SDK_FILE_DOWN_PATH = '/usr/local/nginx/html/ylapp/src/common/libraries/union/files/';
 
 //日志 目录 
-const SDK_LOG_FILE_PATH = '/mnt/myweb/ylapp/src/common/libraries/union/logs/';
+const SDK_LOG_FILE_PATH = '/usr/local/nginx/html/ylapp/src/common/libraries/union/logs/';
 
 //日志级别，关掉的话改PhpLog::OFF
 const SDK_LOG_LEVEL = PhpLog::DEBUG;

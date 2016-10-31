d91058899eed8a20e9d185e266196e2051ecc6e3
moshiyou-home <momo1a@qq.com>
2016-10-31 20:41:44 +0800

    佛挡杀佛的撒范德萨

--- src/static.ylapp.com/admin/js/common.js
+++ src/static.ylapp.com/admin/js/common.js
@@ -1,6 +1,19 @@
 $(function(){
     /*$('.modal-primary').on('hidden.bs.modal', function () {
         $('.modal-primary').text('');*/
+
+    /*解决ckeditor在modal中恐惧不可编辑的问题*/
+    $.fn.modal.Constructor.prototype.enforceFocus = function () {
+        modal_this = this
+        $(document).on('focusin.modal', function (e) {
+            if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
+                    // add whatever conditions you need here:
+                &&
+                !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select') && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
+                modal_this.$element.focus()
+            }
+        })
+    };
 });
 function initFileInput(ctrlName, uploadUrl,maxImageHeight,maxImageWidth) {
     var control = $('#' + ctrlName);

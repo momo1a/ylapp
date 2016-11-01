044e47e57495de63380a58475921dcb1b5f878bb
莫世友 <momo1a@qq.com>
2016-11-01 17:16:54 +0800

    范德萨范德萨

--- src/api.ylapp.com/controllers/notice.php
+++ src/api.ylapp.com/controllers/notice.php
@@ -87,7 +87,7 @@ class Notice extends CI_Controller
 
     //  银联充值通知地址
     public function union_recharge(){
-        file_put_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'union.txt',var_export($_REQUEST,true),FILE_APPEND);
+        //file_put_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'union.txt',var_export($_REQUEST,true),FILE_APPEND);
         //银联支付商户交易号
         $out_trade_no = $_REQUEST['orderId'];
 

2e3ffef14310dc15f7feb4584595767c56e5baf1
莫世友 <momo1a@qq.com>
2016-11-11 08:40:24 +0800

    范德萨范德萨

--- src/admin.ylapp.com/models/user_leaving_msg_model.php
+++ src/admin.ylapp.com/models/user_leaving_msg_model.php
@@ -196,10 +196,10 @@ class User_leaving_msg_model extends MY_Model
 
         if($status == 3){
              // 退款到用户钱包
-            $this->db->query('UPDATE YL_money set `amount`=`amount`+'.$orderInfo['price'].',`updateTime`='.$currentTime.' WHERE `uid`='.$orderInfo['askerUid']);
+           /* $this->db->query('UPDATE YL_money set `amount`=`amount`+'.$orderInfo['price'].',`updateTime`='.$currentTime.' WHERE `uid`='.$orderInfo['askerUid']);
             if($this->db->affected_rows() == 0){
                 $this->db->insert('money',array('uid'=>$orderInfo['askerUid'],'amount'=>$orderInfo['price'],'updateTime'=>$currentTime));
-            }
+            }*/
 
             $this->db->query('UPDATE YL_doctor_reply SET `state`=2 WHERE `type`=1 AND `themeId`='.$oid); // 修改医生回复表状态
         }

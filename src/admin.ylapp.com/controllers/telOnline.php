115847fb7ae984de7798e1774f9dcfcb747b7cd8
莫世友 <momo1a@qq.com>
2016-11-11 11:38:58 +0800

    反倒是发

--- src/admin.ylapp.com/controllers/telOnline.php
+++ src/admin.ylapp.com/controllers/telOnline.php
@@ -39,7 +39,7 @@ class TelOnline extends MY_Controller
         }
         $total = $this->online->countAppoint($keyword,$state);
         $offset = intval($this->uri->segment(3));
-        $list = $this->online->getAppointList($keyword,$limit,$offset,$state,'*,YL_user_phone_diagnosis.id as aid,YL_user_illness_history.sex as asex,YL_user_illness_history.age as aage,YL_user_phone_diagnosis.askTime as atime,YL_user_phone_diagnosis.state as astatus');
+        $list = $this->online->getAppointList($keyword,$limit,$offset,$state,'*,YL_user_phone_diagnosis.id as aid,YL_user_phone_diagnosis.ask_sex as asex,YL_user_illness_history.age as aage,YL_user_phone_diagnosis.askTime as atime,YL_user_phone_diagnosis.state as astatus');
         $page_conf['base_url'] = site_url($this->router->class.'/'.$this->router->method.'/');
         $page_conf['first_url'] = site_url($this->router->class.'/'.$this->router->method.'/0');
         $pager = $this->pager($total, $limit,$page_conf);

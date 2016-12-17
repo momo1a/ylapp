a6d9283311e14d64afe3a66ce09d1ca13188b989
moshiyou-home <momo1a@qq.com>
2016-12-17 21:01:01 +0800

    范德萨发

--- src/common/config/production/shs.php
+++ src/common/config/production/shs.php
@@ -12,7 +12,7 @@ $config['encryption_key'] = '111111'; // 统一密钥,Session加密用到
 
 
 // 上传图片
-$config['upload_image_save_path'] = 'D:\webroot\zzidcconf\nginx\html\images';
+$config['upload_image_save_path'] = 'D:/webroot/zzidcconf/nginx/html/images/';
 $config['upload_image_thumb_size'] = array('illRemark' => array(30,50,80,108), 'leavingMsg' => array(30,50,100,30),'post'=>array(30,60,100,160),'avatar'=>array(30,60,90,108),'certificate'=>array(30,60,90,108),'hospital'=>array(60,98,128),'news'=>array(50,45,98,108));
 $config['upload_image_quality'] = 95;
 
@@ -31,7 +31,7 @@ $config['domain_detail'] = 'http://123.207.88.225:8084/';
 $config['domain_download'] = 'http://123.207.88.225:8085/';  // 下载站点
 
 // app升级包上传目录
-$config['app_update_package_upload_path'] = 'D:\webroot\zzidcconf\nginx\html\upload';
+$config['app_update_package_upload_path'] = 'D:/webroot/zzidcconf/nginx/html/upload/';
 
 $config['super_admin'] = array(1);
 

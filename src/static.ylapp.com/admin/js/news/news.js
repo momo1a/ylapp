69b8da3ad939684483357620d2671f2675b9f9c4
moshiyou-home <momo1a@qq.com>
2017-02-06 09:20:00 +0800

    发的撒发生的佛挡杀佛的是

--- src/static.ylapp.com/admin/js/news/news.js
+++ src/static.ylapp.com/admin/js/news/news.js
@@ -16,7 +16,7 @@ function newsSave(){
     var author = nid == 0 ? $('#news_add input[name="author"]').val() : $('#news_edit input[name="author"]').val();
     var tag = nid == 0 ? $('#news_add input[name="tag"]').val() : $('#news_edit input[name="tag"]').val();
     var content =  nid == 0 ?  CKEDITOR.instances.content.getData() : CKEDITOR.instances.contentEdit.getData();
-    if(!checkInputLength(title,'资讯标题',8,50)){ return false;}
+    //if(!checkInputLength(title,'资讯标题',8,50)){ return false;}
     if(!checkInputLength(author,'作者',2,20)){ return false;}
     if(!checkInputLength(tag,'标签',2,6)){ return false;}
     if(!checkInputLength(content,'正文',20,20000)){ return false;}
